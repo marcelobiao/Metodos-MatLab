47c3fddd35e7068f4a68205f68224809b10e1f39
Marcelo-Desktop <Marcelo-Desktop>
2017-04-09 12:46:30 -0300

    Metodo Bissecao - Alterando equação e intervalo

--- metodoBissecao.m
+++ metodoBissecao.m
@@ -1,7 +1,12 @@
 %Encontra um zero da funcao fx dentro do intervalo [a,b].
-function raiz = metodoBissecao(fx,a,b,precisao)
+function raiz = metodoBissecao()
+    fx=@(x)(x^2)*sin(x)+cos(x);
+    a = -2;
+    b = 2;
+    precisao = 0.01;
+    
     c = (a+b)/2;
-    while(abs(subs(fx,c)) > precisao)
+    while(abs(subs(fx,c))>precisao)
         if(subs(fx,c)*subs(fx,a)>0)
             a = c;
         else
