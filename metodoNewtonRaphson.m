4db0c0bcf2ce7bb93b3b925c624d604b62c8f44d
marcelobiao <marcelobiao2@gmail.com>
2017-04-13 14:18:10 -0300

    Arrumando derivada e criterio de parada metodoNewronRaphson

--- metodoNewtonRaphson.m
+++ metodoNewtonRaphson.m
@@ -1,24 +1,26 @@
 %
 function raiz = metodoNewtonRaphson()
-    syms x(y);
+    syms x;
     fx=(x^2)*sin(x)+cos(x);
-    dfx=functionalDerivative(fx,x);
+    dfx=diff(fx,x);
+
     %TODO: Arrumar derivada
     
-    x0=0.5;
+    x0=-1;
     precisao=0.001;
     Nmax=20;
     
     i=0;
+    raiz=[];
     while(true)
-        x=x0 - (subs(fx,x0)/subs(dfx,x0));
+        x=x0-(subs(fx,x0)/subs(dfx,x0));
         E=abs(x-x0);
         x0=x;
         i=i+1;
         
-        if((E<=precisao) || (subs(fx,x)<=precisao) || (i>=Nmax))
+        raiz=[raiz,x];
+        if((E<=precisao) || (i>=Nmax))
             break;
         end
     end
-    raiz = x;
 end
