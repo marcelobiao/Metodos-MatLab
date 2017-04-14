a1bd133e19252821a4a4c882bab405071fac8de6
marcelobiao <marcelobiao2@gmail.com>
2017-04-14 20:54:13 -0300

    modificando main

--- main.m
+++ main.m
@@ -1,6 +1,8 @@
-%raizIterativoLinear = metodoIterativoLinear();
-%fprintf('RaizLi: %f\n',raizIterativoLinear);
-%raizBissecao = metodoBissecao();
-%fprintf('RaizBi: %f\n',raizBissecao);
+raizIterativoLinear = metodoIterativoLinear();
+fprintf('RaizLi: %f\n',raizIterativoLinear);
+fprintf('new\n');
+raizBissecao = metodoBissecao();
+fprintf('RaizBi: %f\n',raizBissecao);
+fprintf('new\n');
 raizNewtonRaphson = metodoNewtonRaphson();
-fprintf('RaizBi: %f\n',raizNewtonRaphson);
+fprintf('RaizNewtonRaphson: %f\n',raizNewtonRaphson);
