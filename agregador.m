a062f160d044d81931bef0f803795a139b17efde
marcelobiao <marcelobiao2@gmail.com>
2017-04-21 20:04:22 -0300

    Organizando saida

--- agregador.m
+++ agregador.m
@@ -1,6 +1,19 @@
 %Agrega os tres Metodos
 function [raizBissecao, raizIteracao, raizNewton] = agregador()
+    %Executando metodos
+    fprintf('IterativoLinear\n');
     raizIteracao = metodoIterativoLinear();
+    fprintf('Bissecao\n');
     raizBissecao = metodoBissecao();
-    raizNewton = metodoNewtonRapshon();
+    fprintf('NewtonRaphson\n');
+    raizNewton = metodoNewtonRaphson();
+    
+    %Tabela das raizes
+    %TODO: Arrumar raizes na tabela
+    fprintf('new\n');
+    fprintf('RaizLi: %f\n',raizIteracao);
+    fprintf('new\n');
+    fprintf('RaizBi: %f\n',raizBissecao);
+    fprintf('new\n');
+    fprintf('RaizNewtonRaphson: %f\n',raizNewton);
 end
--- main.m
+++ main.m
@@ -1,13 +1,1 @@
-fprintf('IterativoLinear\n');
-raizIterativoLinear = metodoIterativoLinear();
-fprintf('Bissecao\n');
-raizBissecao = metodoBissecao();
-fprintf('NewtonRaphson\n');
-raizNewtonRaphson = metodoNewtonRaphson();
-
-fprintf('new\n');
-fprintf('RaizLi: %f\n',raizIterativoLinear);
-fprintf('new\n');
-fprintf('RaizBi: %f\n',raizBissecao);
-fprintf('new\n');
-fprintf('RaizNewtonRaphson: %f\n',raizNewtonRaphson);
+raizes = agregador();
